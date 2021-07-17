88a8777d477cd9cd993647b05bfb52bd6a8d4eb5
OnLee3 <dlwotjr2@gmail.com>
2021-07-17 22:32:20 +0900

    update invisible click section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-/*중요한 건, 클릭한 구역에 따라 다른 사운드를 출력하고, 기존 사운드의 출력을 멈추는 거다.
+/* 중요한 건, 클릭한 구역에 따라 다른 사운드를 출력하고, 기존 사운드의 출력을 멈추는 거다.
 핵심 기능 구현 후, 디자인 디테일을 추가하자.*/
+
+/* 스트링 겉모습을 잡고, note div를 투명하게 배치하여 클릭요소를 활성화 */
 
 //array 형태
 const note = document.getElementsByClassName("note")
+console.log(note);
+const string = document.getElementsByClassName("string")
 console.log(note);
 
 function findAudio (i){
@@ -10,8 +14,8 @@ const audio = new Audio(`sound/test${i}.wav`);
 return audio
 }
 
-for (let i=0; i<note.length; i++){
-note[i].addEventListener("click", (e)=>{
+for (let i=0; i<string.length; i++){
+string[i].addEventListener("click", (e)=>{
     const audio = findAudio(i+2);
     if (audio.paused) audio.play();
     else {
