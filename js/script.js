d4d279c632656a1b7b781e429584a3315b00fd14
OnLee3 <dlwotjr2@gmail.com>
2021-07-19 21:55:39 +0900

    주석 추가, 변수명 변경

--- js/script.js
+++ js/script.js
@@ -3,19 +3,20 @@
 
 /* 스트링 겉모습을 잡고, note div를 투명하게 배치하여 클릭요소를 활성화 */
 
+
+
 //array 형태
 const note = document.getElementsByClassName("note")
-console.log(note);
-const string = document.getElementsByClassName("string")
-console.log(note);
+const clickBtn = document.getElementsByClassName("clickBtn")
 
 function findAudio (i){
 const audio = new Audio(`sound/test${i}.wav`);
 return audio
 }
 
-for (let i=0; i<string.length; i++){
-string[i].addEventListener("click", (e)=>{
+// 1번줄 개방현부터 i=0 카운트. 마지막 노트 도달할때까지 i++
+for (let i=0; i<clickBtn.length; i++){
+clickBtn[i].addEventListener("click", (e)=>{
     const audio = findAudio(i+2);
     if (audio.paused) audio.play();
     else {
