6a62339240e0ae516460fcc4423b1fd3667d428d
OnLee3 <dlwotjr2@gmail.com>
2021-07-26 19:39:48 +0900

    노트별 음계설정

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
 const note = document.getElementsByClassName("note")
 const clickBtn = document.getElementsByClassName("clickBtn")
 
+
+//Audio 기능
 function findAudio (i){
 const audio = new Audio(`sound/test${i}.wav`);
 return audio
@@ -26,3 +28,52 @@ clickBtn[i].addEventListener("click", (e)=>{
     console.log(`It's ${i}`)
 });
 }
+
+//음계 순서 설정
+const pitch = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+let cnt = 0;
+
+//노트에 음계 부여
+function pitchToNote (){
+    for(let i=8; i<pitch.length; i++){
+clickBtn[cnt].innerText = pitch[i];
+cnt = cnt+1;
+//줄별로 시작피치 설정
+if (cnt === 15) i = 2;
+if (cnt === 30) i = 10;
+if (cnt === 45) i = 5;
+if (cnt === 60) i = 0;
+if (cnt === 75) i = 7;
+if (cnt === 90) break
+if (i === 11) i = -1;
+    }
+}
+pitchToNote();
+
+//스케일 선택 시 해당 음계 색상 변경
+const scaleBtn = document.createElement("select");
+const none = document.createElement("option");
+const penta = document.createElement("option");
+const major = document.createElement("option");
+none.innerText = "None"
+penta.innerText = "Pentatonic Scale"
+major.innerText = "Major Scale"
+scaleBtn.appendChild(none);
+scaleBtn.appendChild(penta);
+scaleBtn.appendChild(major);
+document.body.appendChild(scaleBtn);
+
+function scaleBtnHandler(){
+if(scaleBtn.value === "Pentatonic Scale") {
+    for(i=0; i=note.length; i++){
+        console.log(i)
+        if(clickBtn.innerHTML === "G" || "A" || "C" || "D" || "E" ) console.log("It works");
+    }
+}
+else console.log("Just kidding");
+};
+
+scaleBtn.addEventListener("change", scaleBtnHandler);
+
+
+//코드톤 선택 시 해당 코드톤 색상 변경
